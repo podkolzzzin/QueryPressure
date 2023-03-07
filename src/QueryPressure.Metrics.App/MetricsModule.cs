ad30159b0b90e916331efaeb7cd75af718f0796e
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-03-07 20:33:16 +0200

    add missed files

---
+++ src/QueryPressure.Metrics.App/MetricsModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using QueryPressure.App.Interfaces;
+using QueryPressure.Core;
+using QueryPressure.Core.Interfaces;
+using QueryPressure.Metrics.Core;
+
+[assembly: QueryPressurePlugin]
+
+namespace QueryPressure.Metrics.App;
+
+public class MetricsModule : Module
+{
+  protected override void Load(ContainerBuilder builder)
+  {
+    builder.RegisterType<AverageMetricProvider>()
+      .As<IMetricProvider>()
+      .SingleInstance();
+    
+    builder.RegisterType<StatisticalMetricsProvider>()
+      .As<IMetricProvider>()
+      .SingleInstance();
+
+    builder.RegisterType<LiveAverageMetricProvider>()
+      .As<ILiveMetricProvider>()
+      .InstancePerDependency();
+  }
+}
---
+++ src/QueryPressure.Metrics.Core/SimpleMetric.cs
@@ -0,0 +1,5 @@
+﻿using QueryPressure.Core.Interfaces;
+
+namespace QueryPressure.Metrics.Core;
+
+public record SimpleMetric(string Name, object Value) : IMetric;
