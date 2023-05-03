76a7404ecf0498e26f193fa7d8bcf7b12c615819
Mykhailo Molyboha <viper.jke.mobile@gmail.com>
2023-05-03 19:17:30 +0300

    Change Style for Menu

---
+++ src/QueryPressure.WinUI/Commands/DialogOpenProjectCommand.cs
@@ -0,0 +1,13 @@
+using QueryPressure.WinUI.Common.Commands;
+
+namespace QueryPressure.WinUI.Commands;
+
+public class DialogOpenProjectCommand : CommandBase
+{
+  public override bool CanExecute(object? parameter) => true;
+
+  public override void Execute(object? parameter)
+  {
+    throw new NotImplementedException();
+  }
+}
