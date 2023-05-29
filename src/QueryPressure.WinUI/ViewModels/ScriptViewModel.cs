3bfabd008c82b359717e74e1f691495806b350aa
Mykhailo Molyboha <viper.jke.mobile@gmail.com>
2023-05-29 18:44:13 +0300

    add avalon edit control

--- src/QueryPressure.WinUI/ViewModels/ScriptViewModel.cs
+++ src/QueryPressure.WinUI/ViewModels/ScriptViewModel.cs
@@ -1,4 +1,6 @@
 using System.Windows.Input;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Highlighting;
 using QueryPressure.WinUI.Commands.Scenario;
 using QueryPressure.WinUI.Common.Commands;
 using QueryPressure.WinUI.Models;
@@ -13,8 +15,10 @@ public class ScriptViewModel : PaneViewModel, IDisposable
   private readonly IDisposable _subscription;
   private readonly ILanguageService _languageService;
 
-  private ScenarioModel _model;
-  private string? _script;
+  private ScenarioModel? _model;
+  private TextDocument? _document;
+  private bool _isDirty;
+  private IHighlightingDefinition? _highlightingDefinition;
 
 
   public ScriptViewModel(ISubscriptionManager subscriptionManager, ILanguageService languageService, CloseScenarioScriptCommand closeScenarioScriptCommand, ScenarioModel scenarioModel)
@@ -25,10 +29,8 @@ public class ScriptViewModel : PaneViewModel, IDisposable
       .On(ModelAction.Edit, scenarioModel)
       .Subscribe(OnScenarioChanged);
 
-    _model = scenarioModel;
-
-    ContentId = _model.Id.ToString();
-    OnScenarioChanged(_model);
+    ContentId = scenarioModel.Id.ToString();
+    OnScenarioChanged(scenarioModel);
     CloseCommand = new DelegateCommand(() => closeScenarioScriptCommand.Execute(_model));
   }
 
@@ -41,19 +43,38 @@ public class ScriptViewModel : PaneViewModel, IDisposable
       throw new InvalidOperationException("Content ID has changed");
     }
 
-    Script = _model.Script ?? string.Empty;
+    HighlightingDefinition = GetHighlightingDefinition(_model);
+    Document = new TextDocument(_model.Script ?? string.Empty);
 
     var strings = _languageService.GetStrings();
 
     Title = $"{_model.Name} - {strings["labels.scenario.script"]}";
   }
 
+  private static IHighlightingDefinition GetHighlightingDefinition(ScenarioModel model)
+    => (model.Profile) switch
+    {
+      _ => HighlightingManager.Instance.GetDefinition("TSQL")
+    };
+
   public ICommand CloseCommand { get; }
 
-  public string? Script
+  public TextDocument Document
   {
-    get => _script;
-    set => SetField(ref _script, value);
+    get => _document;
+    set => SetField(ref _document, value);
+  }
+
+  public bool IsDirty
+  {
+    get => _isDirty;
+    set => SetField(ref _isDirty, value);
+  }
+
+  public IHighlightingDefinition HighlightingDefinition
+  {
+    get => _highlightingDefinition;
+    set => SetField(ref _highlightingDefinition, value);
   }
 
   public bool IsEqualTo(ScenarioModel scenarioModel)
