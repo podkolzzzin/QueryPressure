2932170cac4e02976c15195b22e46473cd14c726
Vladyslav Demchenko <vdemchenko99@gmail.com>
2023-07-21 23:11:27 +0200

    create endpoint to cancel running execution

---
+++ src/QueryPressure.UI/BackgroundInfiniteService.cs
@@ -0,0 +1,14 @@
+namespace QueryPressure.UI;
+
+public abstract class BackgroundInfiniteService : BackgroundService
+{
+  protected abstract Task ExecuteRepeatedJobAsync(CancellationToken stoppingToken);
+  
+  protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+  {
+    while (!stoppingToken.IsCancellationRequested)
+    {
+      await ExecuteRepeatedJobAsync(stoppingToken);
+    }
+  }
+}
