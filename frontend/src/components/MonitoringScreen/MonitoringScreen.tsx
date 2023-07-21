353720592f0fa2f9a97fc0de4f8f409e776e4db0
Vladyslav Demchenko <vdemchenko99@gmail.com>
2023-07-21 23:29:54 +0200

    extract components from MonitoringScreen

--- frontend/src/components/MonitoringScreen/MonitoringScreen.tsx
+++ frontend/src/components/MonitoringScreen/MonitoringScreen.tsx
@@ -1,95 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TMonitoringScreenProps } from "./MonitoringScreen.models";
-import { HubConnectionBuilder, HubConnection } from '@microsoft/signalr';
 import { Arrow } from "../Arrow";
-import ChartJS from 'chart.js/auto'; // import everything
-
-let connection: HubConnection | null = null;
-
-type EventHandlerSet = {
-    setRequestCountMetric: (value: number) => void,
-    setAverageMetric: (value: number) => void,
-    notifyExecutionCompleted: (isSuccessful: boolean, message: string | null) => void;
-    newConnectionEstablished: () => void,
-};
-
-type TChartProps = { 
-    title: string, 
-    data: { x: number, y: number }[],
-    fullWidth?: boolean, 
-    fullHeight?: boolean 
-}
-
-const setupConnection = (id: string, handlers: EventHandlerSet) => {
-    // resubscribe if connection exist
-    if (connection) connection.stop();
-
-    connection = new HubConnectionBuilder()
-        .withUrl(`/ws/dashboard?executionId=${id}`)
-        .withAutomaticReconnect()
-        .build();
-
-    connection.on('live-average', (metric) => handlers.setAverageMetric(metric.nanoseconds));
-    connection.on('live-request-count', handlers.setRequestCountMetric);
-    connection.on('execution-completed', ({ isSuccessful, message }) =>
-        handlers.notifyExecutionCompleted(isSuccessful, message));
-
-    connection.start()
-        .then(() => {
-            console.log('SignalR connection established');
-            handlers.newConnectionEstablished();
-        })
-        .catch((error) => {
-            console.error('SignalR connection error:', error);
-        });
-};
-
-const Chart = ({ title, data, fullWidth, fullHeight }: TChartProps) => {
-    const id = `myChart-${title}`
-    useEffect(() => {
-        const chart = new ChartJS(id, {
-            type: 'line',
-            data: {
-                labels: data.map(x => x.x),
-                datasets: [{
-                    label: title,
-                    data: data.map(x => x.y),
-                    fill: false,
-                    borderColor: 'rgb(75, 192, 192)',
-                    tension: 0
-                }]
-            },
-            options: {
-                animation: {
-                    duration: 0
-                },
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                    },
-                    x: {
-                        display: false,
-                    }
-                },
-            }
-        });
-        return () => {
-            chart.destroy()
-        }
-    }, [data])
-
-    return (<div className={`card ${fullHeight && 'h-100'}`} style={{ width: fullWidth ? '100%' : '49%' }}>
-        <div className="card-header">{title}</div>
-        <div className="card-body d-flex justify-content-center">
-            <canvas id={id}></canvas>
-        </div>
-    </div>)
-};
+import { LineChart } from '../LineChart';
+import { EventHandlerSet, subsctibeToExecutionEvents } from '@/services';
 
 export const MonitoringScreen = ({ executionId, showMonitor, toggleMonitor, toggleCancelButton }: TMonitoringScreenProps) => {
     const [fullView, setFullView] = useState(false);
@@ -101,8 +14,8 @@ export const MonitoringScreen = ({ executionId, showMonitor, toggleMonitor, togg
     const [averageChartData, setAverageChartData] = useState<{ x: number, y: number }[]>([]);
 
     const eventHandlers: EventHandlerSet = {
-        setRequestCountMetric: setRequestCountMetric,
-        setAverageMetric: setAverageMetric,
+        requestCountMetricReceived: setRequestCountMetric,
+        averageMetricReceived: setAverageMetric,
         notifyExecutionCompleted: (isSuccessful: boolean, message: string | null) => { 
             toggleCancelButton(false);
         },
@@ -115,7 +28,7 @@ export const MonitoringScreen = ({ executionId, showMonitor, toggleMonitor, togg
 
     useEffect(() => {
         if (!executionId) return;
-        setupConnection(executionId, eventHandlers);
+        subsctibeToExecutionEvents(executionId, eventHandlers);
     }, [executionId]);
 
 
@@ -151,8 +64,8 @@ export const MonitoringScreen = ({ executionId, showMonitor, toggleMonitor, togg
             </div>
             {showMonitor &&
                 <div className={`d-flex h-100 ${!fullView ? 'flex-wrap' : 'flex-column'}`} style={{ gap: '2%', margin: '0 10px 10px' }}>
-                    <Chart title='Average' fullWidth={fullView} fullHeight={fullView} data={averageChartData} />
-                    <Chart title='Request Count' fullWidth={fullView} fullHeight={fullView} data={requestCountChartData} />
+                    <LineChart title='Average' fullWidth={fullView} fullHeight={fullView} data={averageChartData} />
+                    <LineChart title='Request Count' fullWidth={fullView} fullHeight={fullView} data={requestCountChartData} />
                     {/* <Chart title='RPS / Error rate' fullWidth fullHeight={fullView} /> */}
                 </div>
             }
--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -2,3 +2,4 @@ export * from './ConnectionService';
 export * from './ConnectionValidationService';
 export * from './LocalStorageService';
 export * from './ProviderService';
+export * from './SignalRService';
