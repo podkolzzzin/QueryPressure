30360e52254fa5d3bbda78372ccf92bb705b821e
Andrii Podkolzin <andrii.podkolzin@namecheap.com>
2023-07-31 15:26:17 +0300

    fix: store args in URL

--- frontend/src/hooks/useProfile.ts
+++ frontend/src/hooks/useProfile.ts
@@ -1,26 +1,35 @@
-import {useEffect, useState} from 'react';
-import {ProfilesApi} from '@api';
-import {ProfileModel} from '@models';
+import { useEffect, useState } from 'react';
+import { ProfilesApi } from '@api';
+import { ProfileModel } from '@models';
+
+import { UrlService } from '@/services/UrlService';
 
 export function useProfile() {
-  const [selectedProfile, setSelectedProfile] = useState<ProfileModel | null>(null);
+  const [selectedProfile, setSelectedProfile] = useState<ProfileModel | null>();
   const [profiles, setProfiles] = useState<ProfileModel[]>([]);
 
   function selectProfile(profileType: string) {
     const profile: ProfileModel = profiles.find(p => p.type === profileType)!;
     setSelectedProfile(profile);
-  }
-
-  function loadProfiles(): void {
-    ProfilesApi
-      .getAll()
-      .then(profiles => setProfiles(profiles));
+    UrlService.set('profile', profileType);
   }
 
   useEffect(() => {
+    function loadProfiles(): void {
+      ProfilesApi
+        .getAll()
+        .then(profiles => {
+          setProfiles(profiles);
+          // After the profiles have been loaded, check if a 'profile' parameter exists in the URL
+          const urlProfileType = UrlService.get('profile');
+          // If it does exist, select the profile with that type
+          if (urlProfileType) {
+            selectProfile(urlProfileType);
+          }
+        });
+    }
     loadProfiles();
-  }, [setProfiles]);
-
+  });
 
   return {
     profiles,
